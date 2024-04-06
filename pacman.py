9633bc3f59adb6140e1b610fe7877d6c1dfe8702
Brady Rhora <brady0423@gmail.com>
2024-04-06 17:10:18 -0400

    added movement

--- pacman.py
+++ pacman.py
@@ -1,7 +1,12 @@
+from pydirectinput import press
+import random
+
 class Game:
     def __init__(self):
         self.entities = []
+        self.board = []
         self.pacman = None
+        self.bounds = None
 
     def reset_entities(self):
         self.entities_last = self.entities
@@ -13,13 +18,81 @@ class Game:
         if entity.type == 'pacman':
             self.pacman = entity
 
-    def get_ghost(self, name):
-        for entity in self.entities:
-            if entity.type == 'ghost' and entity.name == name:
-                return entity
+    def add_node(self, pos):
+        node = self.get_node(pos)
+
+        if node is None:
+            node = Map_Node(pos)
+            self.board.append(node)
+        return node
+    
+    def get_node(self, pos):
+        for node in self.board:
+            if abs(node.pos[0] - pos[0]) < 10 and abs(node.pos[1] - pos[1]) < 10:
+                return node
         return None
+    
+    def get_closest_node(self, pos):
+        closest = None
+        closest_dist = 1000000
+        for node in self.board:
+            dist = abs(node.pos[0] - pos[0]) + abs(node.pos[1] - pos[1])
+            if dist < closest_dist:
+                closest = node
+                closest_dist = dist
+        return closest
 
+    def plot_path(self, entity_start, entity_end):
+        if entity_start is None or entity_end is None:
+            return None
+        start = self.get_closest_node(entity_start.pos)
+        end = self.get_closest_node(entity_end.pos)
+        path = self.a_star(start, end)
+        return path
+    
+    def a_star(self, start, end):
+        open_list = []
+        closed_list = []
+        open_list.append(start)
+        start.g = 0
+        start.h = start.distance(end)
+        start.f = start.h
+        bad_nodes = [self.get_closest_node(entity.pos) for entity in self.entities_last if entity.type == 'ghost']
 
+        while len(open_list) > 0:
+            current = open_list[0]
+            for node in open_list:
+                if node.f < current.f:
+                    current = node
+            open_list.remove(current)
+            closed_list.append(current)
+
+            if current == end:
+                path = []
+                while current != start:
+                    path.append(current)
+                    current = current.parent
+                path.append(start)
+                path.reverse()
+                return path
+
+            for neighbour in current.get_neighbours():
+                if neighbour in closed_list or neighbour in bad_nodes:
+                    continue
+                if neighbour not in open_list:
+                    open_list.append(neighbour)
+                    neighbour.g = current.g + 1
+                    neighbour.h = neighbour.distance(end)
+                    neighbour.f = neighbour.g + neighbour.h
+                    neighbour.parent = current
+                else:
+                    if current.g + 1 < neighbour.g:
+                        neighbour.g = current.g + 1
+                        neighbour.f = neighbour.g + neighbour.h
+                        neighbour.parent = current
+        return None
+    
+    
 class Entity:
     def __init__(self, pos):
         self.pos = pos
@@ -29,7 +102,7 @@ class Entity:
     def distance(self, entity):
         return abs(entity.pos[0] - self.pos[0]) + abs(entity.pos[1] - self.pos[1])
 
-    def get_neighbours(self, radius = 50):
+    def get_neighbours(self, radius = 120, increment = 50):
         neighbours = []
         while len(neighbours) == 0:
             for entity in self.game.entities:
@@ -37,9 +110,37 @@ class Entity:
                     distance = self.distance(entity)
                     if distance < radius:
                         neighbours.append((entity, distance))
-            radius += 50
+            radius += increment
+            if radius > (self.game.bounds[1][1] - self.game.bounds[0][1]):
+                break
         return neighbours
     
+
+class Map_Node(Entity):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.type = 'node'
+        self.neighbour_L = None
+        self.neighbour_R = None
+        self.neighbour_U = None
+        self.neighbour_D = None
+        self.g = float('inf')
+        self.h = 0
+        self.f = 0
+        self.parent = None
+
+    def get_neighbours(self):
+        neighbours = []
+        if self.neighbour_L is not None:
+            neighbours.append(self.neighbour_L)
+        if self.neighbour_R is not None:
+            neighbours.append(self.neighbour_R)
+        if self.neighbour_U is not None:
+            neighbours.append(self.neighbour_U)
+        if self.neighbour_D is not None:
+            neighbours.append(self.neighbour_D)
+        return neighbours
+
 class Pacman(Entity):
     def __init__(self, pos):
         super().__init__(pos)
@@ -47,8 +148,27 @@ class Pacman(Entity):
 
     def move(self):
         neighbours = self.get_neighbours()
-        # sort by dist
-        neighbours.sort(key=lambda x: x[1])
+        neighbours.sort(key = lambda x: x[0].value, reverse = True)
+        if len(neighbours) == 0:
+            return None
+
+        my_node = self.game.get_closest_node(self.pos)
+        path = self.game.plot_path(self, neighbours[0][0])
+        if path is not None:
+            if len(path) == 1:
+                return None
+            next_node = path[1]
+            if next_node.pos[0] < my_node.pos[0]:
+                press('left')
+            elif next_node.pos[0] > my_node.pos[0]:
+                press('right')
+            elif next_node.pos[1] < my_node.pos[1]:
+                press('up')
+            elif next_node.pos[1] > my_node.pos[1]:
+                press('down')
+        
+        return path
+        
 
 class Pellet(Entity):
     def __init__(self, pos):
@@ -62,9 +182,8 @@ class PowerUp(Entity):
         self.value = 3
 
 class Ghost(Entity):
-    def __init__(self, pos, name):
+    def __init__(self, pos):
         super().__init__(pos)
-        self.name = name
         self.type = 'ghost'
         self.value = -1
 
--- program.py
+++ program.py
@@ -3,6 +3,7 @@ import numpy as np
 from PIL import ImageGrab as ig
 from pynput import mouse
 import pacman
+import time
 
 game = pacman.Game()
 ideal_bounds = [(670, 255), (1250, 920)]
@@ -13,12 +14,14 @@ colors = {
     'power': (0, 255, 255),
     'cherry': (255, 0, 0),
     'pacman': (0, 255, 0),
-    'pacman-open': (0, 255, 0),
+    'pacman_open': (0, 255, 0),
+    'pacman_open_2': (0, 255, 0),
     'pinky': (0, 0, 255),
     'blinky': (0, 0, 255),
     'inky': (0, 0, 255),
     'clyde': (0, 0, 255),
-    'vuln_ghost': (255, 0, 255)
+    'vuln_ghost': (255, 0, 255),
+    'vuln_ghost_2': (255, 0, 255)
 }
 
 def init_game_bounds():
@@ -29,6 +32,7 @@ def init_game_bounds():
         else:
             # mouse up
             game_bounds[1] = (x, y)
+            game.bounds = game_bounds
             return False
 
     print('Please click and drag over the game bounds.')
@@ -45,7 +49,7 @@ def get_screen():
     return img
 
 def load_templates():
-    templates = ['pacman', 'pacman-open', 'pinky', 'blinky', 'inky', 'clyde', 'power', 'cherry', 'pellet', 'vuln_ghost']
+    templates = ['pacman', 'pacman_open', 'pacman_open_2', 'pinky', 'blinky', 'inky', 'clyde', 'power', 'cherry', 'pellet', 'vuln_ghost', 'vuln_ghost_2']
     global template_images
     template_images = {}
     for template in templates:
@@ -54,46 +58,117 @@ def load_templates():
             print(f'Error: Could not load template {template}.png')
             exit(1)
 
-def detect_objects(screen):
+def load_board():
+    board = cv.imread('board.png')
+    detect_objects(board, load_board=True)
+
+def detect_objects(screen, load_board = False):
     game.reset_entities()
 
     for template_name, template_image in template_images.items():
         result = cv.matchTemplate(screen, template_image, cv.TM_SQDIFF_NORMED)
-        if template_name in ['pacman', 'pacman-open', 'cherry', 'inky', 'blinky', 'pinky', 'clyde']: # single object
+        if template_name in ['pacman', 'pacman_open', 'cherry', 'inky', 'blinky', 'pinky', 'clyde']: # single object
             if result.min() < 0.3:
                 _, _, min_loc, _ = cv.minMaxLoc(result)
                 top_left = min_loc
                 h, w = template_image.shape[:2]
                 bottom_right = (top_left[0] + w, top_left[1] + h)
+                center = (top_left[0] + w // 2, top_left[1] + h // 2)
                 
                 if template_name in ['inky', 'blinky', 'pinky', 'clyde']:
-                    game.add_entity(pacman.Ghost(top_left, template_name))
-                elif template_name in ['pacman', 'pacman-open']:
-                    game.add_entity(pacman.Pacman(top_left))
+                    game.add_entity(pacman.Ghost(center, template_name))
+                elif template_name in ['pacman', 'pacman_open', 'pacman_open_2']:
+                    game.add_entity(pacman.Pacman(center))
 
-                cv.rectangle(screen, top_left, bottom_right, colors[template_name], 1)
-                cv.putText(screen, template_name, top_left, cv.FONT_HERSHEY_SIMPLEX, 0.5, colors[template_name], 1)
+                #cv.rectangle(screen, top_left, bottom_right, colors[template_name], 1)
+                #cv.putText(screen, template_name, top_left, cv.FONT_HERSHEY_SIMPLEX, 0.5, colors[template_name], 1)
         else: # multiple objects
             loc = np.where(result < 0.15)
             for pt in zip(*loc[::-1]):
                 top_left = pt
                 h, w = template_image.shape[:2]
                 bottom_right = (top_left[0] + w, top_left[1] + h)
+                center = (top_left[0] + w // 2, top_left[1] + h // 2)
 
                 if template_name == 'pellet':
-                    game.add_entity(pacman.Pellet(top_left))
+                    game.add_entity(pacman.Pellet(center))
                 elif template_name == 'power': # FIX: PowerUps being detected multiple times for some reason
-                    game.add_entity(pacman.PowerUp(top_left))
-                elif template_name == 'vuln_ghost':
-                    game.add_entity(pacman.VulnerableGhost(top_left))
+                    game.add_entity(pacman.PowerUp(center))
+                elif template_name == 'vuln_ghost' or template_name == 'vuln_ghost_2':
+                    game.add_entity(pacman.VulnerableGhost(center))
                 
-                cv.rectangle(screen, top_left, bottom_right, colors[template_name], 1)
+                #cv.rectangle(screen, top_left, bottom_right, colors[template_name], 1)
+    if load_board:
+        for e in game.entities:
+            if e.type == 'pellet' or e.type == 'power' or e.type == 'pacman':
+                node = game.add_node(e.pos)
+
+                n = e.get_neighbours(50, 10)
+                for (entity, dist) in n:
+                    if entity.type == 'pellet' or entity.type == 'power' or entity.type == 'pacman':
+                        if abs(entity.pos[0] - e.pos[0]) < 10 or abs(entity.pos[1] - e.pos[1] < 10):
+                            node2 = game.add_node(entity.pos)
+                            if node == node2:
+                                continue
+
+                            if entity.pos[0] == e.pos[0]:
+                                if entity.pos[1] < e.pos[1]:
+                                    node.neighbour_U = node2
+                                else:
+                                    node.neighbour_D = node2
+                            else:
+                                if entity.pos[0] < e.pos[0]:
+                                    node.neighbour_L = node2
+                                else:
+                                    node.neighbour_R = node2
+
+def detect_objects_optimized(screen, load_board = False):
+    game.reset_entities()
+
+    # Map template names to entity classes
+    entity_classes = {
+        'inky': pacman.Ghost,
+        'blinky': pacman.Ghost,
+        'pinky': pacman.Ghost,
+        'clyde': pacman.Ghost,
+        'pacman': pacman.Pacman,
+        'pacman_open': pacman.Pacman,
+        'pacman_open_2': pacman.Pacman,
+        'pellet': pacman.Pellet,
+        'power': pacman.PowerUp,
+        'vuln_ghost': pacman.VulnerableGhost,
+        'vuln_ghost_2': pacman.VulnerableGhost
+    }
+
+    for template_name, template_image in template_images.items():
+        result = cv.matchTemplate(screen, template_image, cv.TM_SQDIFF_NORMED)
+        h, w = template_image.shape[:2]
+
+        if template_name in ['pacman', 'pacman_open', 'cherry', 'inky', 'blinky', 'pinky', 'clyde']: # single object
+            if result.min() < 0.3:
+                _, _, min_loc, _ = cv.minMaxLoc(result)
+                top_left = min_loc
+                bottom_right = (top_left[0] + w, top_left[1] + h)
+                center = (top_left[0] + w // 2, top_left[1] + h // 2)
+                
+                # Add the entity to the game
+                game.add_entity(entity_classes[template_name](center))
+
+        else: # multiple objects
+            loc = np.where(result < 0.15)
+            for pt in zip(*loc[::-1]):
+                top_left = pt
+                bottom_right = (top_left[0] + w, top_left[1] + h)
+                center = (top_left[0] + w // 2, top_left[1] + h // 2)
+
+                # Add the entity to the game
+                game.add_entity(entity_classes[template_name](center))
 
 def move_pacman():
     if (game.pacman is None):
         return
     
-    game.pacman.move()
+    return game.pacman.move()
 
 def draw_neighbours(screen):
     if (game.pacman is None):
@@ -107,12 +182,28 @@ def draw_neighbours(screen):
 def main():
     init_game_bounds()
     load_templates()
+    load_board()
 
     while True:
+        stime = time.time()
+        atime = time.time()
         screen = get_screen()
-        detect_objects(screen)
-        draw_neighbours(screen)
-
+        print(f'Get screen: {time.time() - stime}')
+        stime = time.time()
+        detect_objects_optimized(screen)
+        print(f'Detect objects: {time.time() - stime}')
+        stime = time.time()
+        print(f'Draw neighbours: {time.time() - stime}')
+        stime = time.time()
+        path = move_pacman()
+        stime = time.time()
+        print(f'Move pacman: {time.time() - stime}')
+        if path is not None:
+            for i in range(len(path) - 1):
+                cv.line(screen, path[i].pos, path[i + 1].pos, (0, 255, 255), 2)
+        #print(f'Move pacman: {time.time() - stime}')
+        #print(f'Total time: {time.time() - atime}')
+        
         cv.imshow('screen', screen)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
